c51625315812ec68362dfe945ff01a5875a09f2a
Tan Bui Nhat <buinhattan28@gmail.com>
2023-09-15 18:36:39 -0500

    fix training file

--- training_scripts/run_training_lung.sh
+++ training_scripts/run_training_lung.sh
@@ -7,4 +7,4 @@ export RESULTS_FOLDER=.../output_lung
 export sam3d_preprocessed="$DATASET_PATH"/sam3d_raw/sam3d_raw_data/Task06_Lung
 export sam3d_raw_data_base="$DATASET_PATH"/sam3d_raw
 
-python .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_lung 6 0 
+python .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_lung 6 0 --vit_name vit_b 
--- training_scripts/run_training_synapse.sh
+++ training_scripts/run_training_synapse.sh
@@ -7,4 +7,4 @@ export RESULTS_FOLDER=.../output_synapse
 export sam3d_preprocessed="$DATASET_PATH"/sam3d_raw/sam3d_raw_data/Task02_Synapse
 export sam3d_raw_data_base="$DATASET_PATH"/sam3d_raw
 
-python3 .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_synapse 2 0
+python3 .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_synapse 2 0 --vit_name vit_b 
