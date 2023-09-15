4dd722f6b8d7a75e34c83d95d0123306189dd839
Nhat-Tan Bui <60350737+tanbuinhat@users.noreply.github.com>
2023-09-15 18:45:47 -0500

    Update run_evaluation_synapse.sh

--- evaluation_scripts/run_evaluation_synapse.sh
+++ evaluation_scripts/run_evaluation_synapse.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-DATASET_PATH=.../DATASET_Synapse
-CHECKPOINT_PATH=.../DATASET_Synapse/output_synapse
+DATASET_PATH=../DATASET_Synapse
+CHECKPOINT_PATH=../DATASET_Synapse/output_synapse
 
 export PYTHONPATH=.././
 export RESULTS_FOLDER="$CHECKPOINT_PATH"
@@ -9,4 +9,4 @@ export sam3d_preprocessed="$DATASET_PATH"/sam3d_raw/sam3d_raw_data/Task02_Synaps
 export sam3d_raw_data_base="$DATASET_PATH"/sam3d_raw
 
 
-python .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_synapse 2 0 -val --valbest
+python ../sam3d/run/run_training.py 3d_fullres sam3d_trainer_synapse 2 0 -val --valbest
