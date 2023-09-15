8ce8a5e0fe5244c3853cf1b6f1c6c461a25b52cb
Nhat-Tan Bui <60350737+tanbuinhat@users.noreply.github.com>
2023-09-15 18:46:00 -0500

    Update run_evaluation_tumor.sh

--- evaluation_scripts/run_evaluation_tumor.sh
+++ evaluation_scripts/run_evaluation_tumor.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-DATASET_PATH=.../DATASET_Tumor
-CHECKPOINT_PATH=.../DATASET_Tumor/output_tumor
+DATASET_PATH=../DATASET_Tumor
+CHECKPOINT_PATH=../DATASET_Tumor/output_tumor
 
 export PYTHONPATH=.././
 export RESULTS_FOLDER="$CHECKPOINT_PATH"
 export sam3d_preprocessed="$DATASET_PATH"/sam3d_raw/sam3d_raw_data/Task03_tumor
 export sam3d_raw_data_base="$DATASET_PATH"/sam3d_raw
 
-python .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_tumor 3 0 -val --valbest 
+python ../sam3d/run/run_training.py 3d_fullres sam3d_trainer_tumor 3 0 -val --valbest 
 
