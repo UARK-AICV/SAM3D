978a14aae1acde675e28b54de970c4080ee19335
Nhat-Tan Bui <60350737+tanbuinhat@users.noreply.github.com>
2023-09-15 18:46:29 -0500

    Update run_training_lung.sh

--- training_scripts/run_training_lung.sh
+++ training_scripts/run_training_lung.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DATASET_PATH=.../DATASET_Lungs
+DATASET_PATH=../DATASET_Lungs
 
 export PYTHONPATH=.././
-export RESULTS_FOLDER=.../output_lung
+export RESULTS_FOLDER=../output_lung
 export sam3d_preprocessed="$DATASET_PATH"/sam3d_raw/sam3d_raw_data/Task06_Lung
 export sam3d_raw_data_base="$DATASET_PATH"/sam3d_raw
 
-python .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_lung 6 0 --vit_name vit_b 
+python ../sam3d/run/run_training.py 3d_fullres sam3d_trainer_lung 6 0 --vit_name vit_b 
