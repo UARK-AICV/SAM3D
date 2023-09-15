23a08363a75dc5820725341fecedb367dd11e4ab
Nhat-Tan Bui <60350737+tanbuinhat@users.noreply.github.com>
2023-09-15 18:46:15 -0500

    Update run_training_acdc.sh

--- training_scripts/run_training_acdc.sh
+++ training_scripts/run_training_acdc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DATASET_PATH=.../DATASET_Acdc
+DATASET_PATH=../DATASET_Acdc
 
 export PYTHONPATH=.././
-export RESULTS_FOLDER=.../output_acdc
+export RESULTS_FOLDER=../output_acdc
 export sam3d_preprocessed="$DATASET_PATH"/sam3d_raw/sam3d_raw_data/Task01_ACDC
 export sam3d_raw_data_base="$DATASET_PATH"/sam3d_raw
 
-python .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_acdc 1 0 --vit_name vit_b 
+python ../sam3d/run/run_training.py 3d_fullres sam3d_trainer_acdc 1 0 --vit_name vit_b 
