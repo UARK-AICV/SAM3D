e6c4d1fe3bd8626bc94ea7895dce48d532e6368b
Nhat-Tan Bui <60350737+tanbuinhat@users.noreply.github.com>
2023-09-15 18:46:52 -0500

    Update run_training_tumor.sh

--- training_scripts/run_training_tumor.sh
+++ training_scripts/run_training_tumor.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DATASET_PATH=.../DATASET_Tumor
+DATASET_PATH=../DATASET_Tumor
 
 export PYTHONPATH=.././
-export RESULTS_FOLDER=.../output_tumor
+export RESULTS_FOLDER=../output_tumor
 export sam3d_preprocessed="$DATASET_PATH"/sam3d_raw/sam3d_raw_data/Task03_tumor
 export sam3d_raw_data_base="$DATASET_PATH"/sam3d_raw
 
-python3 .../sam3d/run/run_training.py 3d_fullres sam3d_trainer_tumor 3 0 --vit_name vit_b 
+python3 ../sam3d/run/run_training.py 3d_fullres sam3d_trainer_tumor 3 0 --vit_name vit_b 
